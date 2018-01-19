f0acd409b233b7d4cd8478d0c4d349f757f6a8e5
kwin555 <ktn555@att.net>
2018-01-19 22:36:00 +0000

    v16 added a show method in the controller to show the new page a retaurant is clicked

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -3,7 +3,7 @@ class PlacesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
   def index
-    @places = Place.paginate(:page => params[:page], per_page: 5)
+    @places = Place.paginate(:page => params[:page], :per_page => 2)
   end
 
   def new
@@ -13,6 +13,10 @@ class PlacesController < ApplicationController
   def create
     current_user.places.create(place_params)
     redirect_to root_path
+  end
+
+  def show
+    @place = Place.find(params[:id])
   end
 
   private
