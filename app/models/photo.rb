e38c4d358537e9fd2c6e349a5133699e0f6b63c3
kwin555 <ktn555@att.net>
2018-01-21 02:05:31 +0000

    v30 added: pictures to index and the ability to upload pictures to restaurants

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,8 +2,6 @@ class Photo < ApplicationRecord
   belongs_to :place
   mount_uploader :picture, PictureUploader
 
-  def new 
-    @photo = Photo.new
-  end
-  
+
+
 end
--- app/models/place.rb
+++ app/models/place.rb
@@ -2,12 +2,13 @@ class Place < ApplicationRecord
   belongs_to :user
   has_many :comments
   has_many :photos
+  
 
 
   geocoded_by :address
   after_validation :geocode
   
-  validates :name, :caption, presence: true, length: { minimum: 4 }
+  validates :name, presence: true, length: { minimum: 4 }
   validates :address, :description, presence: true
   
 end
