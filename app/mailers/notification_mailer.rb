c1fc15342c1890ba8071c5b31c4b08903539c4b9
kwin555 <ktn555@att.net>
2018-01-21 23:41:14 +0000

    intermittent fix for email

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -3,7 +3,6 @@ class NotificationMailer < ApplicationMailer
 
   def comment_added
     mail(to: "kev.ftw595@gmail.com",
-      subject: "Acomment has been added to your place")
-
+         subject: "A comment has been added to your place")
   end
 end
