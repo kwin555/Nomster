e973d8c41eb9d769bdb2e33599dcf8ac87018215
kwin555 <ktn555@att.net>
2018-01-22 00:34:35 +0000

    v31 finishing touches on email to personal account

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,7 +2,7 @@ class NotificationMailer < ApplicationMailer
   default from: "no-reply@nomsterapp.com"
 
   def comment_added
-    mail(to: "kev.ftw595@gmail.com",
+    mail(to: "ktn555@att.net",
          subject: "A comment has been added to your place")
   end
 end
